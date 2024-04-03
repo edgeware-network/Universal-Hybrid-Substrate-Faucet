8b66ba12370887da49f0e2bd6e4257ce06562908
pranavcpawar <pranavcpawar@outlook.com>
2024-04-03 14:46:12 +0530

    add connect wallet

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ConnectWallet = (): React.JSX.Element => {
   return (
-    <div>ConnectWallet</div>
-  );
+    <button className="flex items-center justify-center px-4 py-2 bg-[#311C31] text-[#FC72FF] sm:text-base text-sm font-medium rounded-[10px] active:scale-95">Connect Wallet</button>
+    );
 };
 
 export default ConnectWallet;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = (): React.JSX.Element => {
   return (
     <div className='max-w-[1204px] w-[90vw] fixed bottom-5 z-10 h-14 flex flex-col items-center text-wrap justify-center bg-[#1b1b1b] font-medium rounded-[10px]'>
-      <p className='text-xs font-medium text-[#656565] flex items-center justify-center'>Built and maintain by Edgeware community</p>
+      <p className='text-xs font-medium text-[#656565] flex items-center justify-center'>Made by Edgeware contributors</p>
       <p className='text-xs font-medium text-[#494949]'>Edgeware © 2024 - {new Date().getFullYear()}</p>
     </div>
   );
