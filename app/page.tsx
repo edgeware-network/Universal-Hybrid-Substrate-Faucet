f9a983556c70fbb5963b559971b6a92d6c4731f9
pranavcpawar <pranavcpawar@outlook.com>
2024-04-28 15:48:18 +0530

    bugfix: captcha toast when not connected

--- app/page.tsx
+++ app/page.tsx
@@ -59,21 +59,23 @@ export default function Home() {
     event.preventDefault();
     captchaRef.current?.execute();
     console.log(user);
-    toast.custom((t) => (
-      <div
-        className={`${
-          t.visible ? 'animate-enter' : 'animate-leave'
-        } max-w-[512px] w-full bg-[#1b1b1b] shadow-lg rounded-lg items-center border-[#404040] justify-center gap-[10px] pointer-events-auto flex ring-1 p-4 ring-black ring-opacity-5`}
-      >
-        <LuCheckSquare className="text-green-500 h-5 w-5"/>
-        <span className="h-2 w-2 mr-2 block shrink-0" />
-        {toggle 
-          ? <p className="text-sm">Successfully, sent {user.amount} {chains.find((a) => a.name === user.chain)?.nativeCurrency.symbol} to your wallet address </p>
-          : <p className="text-sm">Successfully, sent {user.amount} to your selected chains</p>
-        }
-      </div>
-    ))
-    setUser({ ...user, amount: "", chain: "", address: "" });
+    if(state.ethereumConnected || state.polkadotConnected){
+      toast.custom((t) => (
+        <div
+          className={`${
+            t.visible ? 'animate-enter' : 'animate-leave'
+          } max-w-[512px] w-full bg-[#1b1b1b] shadow-lg rounded-lg items-center border-[#404040] justify-center gap-[10px] pointer-events-auto flex ring-1 p-4 ring-black ring-opacity-5`}
+        >
+          <LuCheckSquare className="text-green-500 h-5 w-5"/>
+          <span className="h-2 w-2 mr-2 block shrink-0" />
+          {toggle 
+            ? <p className="text-sm">Successfully, sent {user.amount} {chains.find((a) => a.name === user.chain)?.nativeCurrency.symbol} to your wallet address </p>
+            : <p className="text-sm">Successfully, sent {user.amount} to your selected chains</p>
+          }
+        </div>
+      ))
+      setUser({ ...user, amount: "", chain: "", address: "" });
+    }
     router.push("/");
   };
 
