c10e129d5a2d4ab1ec06d2364dc4c5f526fcc3b9
Rama Vats <guy55222@gmail.com>
2024-05-08 11:23:21 +0530

    changed network selector to pill sized

--- components/modals/Switch.tsx
+++ components/modals/Switch.tsx
@@ -1,26 +1,35 @@
 "use client";
-import React, { FC, useState } from 'react';
-import { RxCross2 } from 'react-icons/rx';
+import React, { FC, useState } from "react";
+import { RxCross2 } from "react-icons/rx";
 import { FiSearch } from "react-icons/fi";
 import { GiCycle } from "react-icons/gi";
-import { useFaucetContext } from '@/context';
-import { Switch as HeadlessSwitch } from '@headlessui/react';
-import { Chain, chains } from '@/constants/config';
-import Switcher from '../widgets/Switcher';
-import Selector from '../widgets/Selector';
-import { useRouter } from 'next/navigation';
+import { useFaucetContext } from "@/context";
+import { Switch as HeadlessSwitch } from "@headlessui/react";
+import { Chain, chains } from "@/constants/config";
+import Switcher from "../widgets/Switcher";
+import Selector from "../widgets/Selector";
+import { useRouter } from "next/navigation";
 
 interface SwitchModalProps {
   onClose: () => void;
-};
+}
 
-const Switch: FC<SwitchModalProps> = ({onClose}: SwitchModalProps) => {
-  const { state, switchEthereumChain, switchPolkadotChain, toggle, setToggle, setUser, user } = useFaucetContext();
+const Switch: FC<SwitchModalProps> = ({ onClose }: SwitchModalProps) => {
+  const {
+    state,
+    switchEthereumChain,
+    switchPolkadotChain,
+    toggle,
+    setToggle,
+    setUser,
+    user,
+  } = useFaucetContext();
   const [queryChain, setQueryChain] = useState("");
   const router = useRouter();
 
   const handleClose = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target instanceof HTMLDivElement && e.target.id  === 'switch_modal') onClose();
+    if (e.target instanceof HTMLDivElement && e.target.id === "switch_modal")
+      onClose();
   };
 
   const handleSwitch = (chain: Chain) => {
@@ -30,63 +39,85 @@ const Switch: FC<SwitchModalProps> = ({onClose}: SwitchModalProps) => {
     if (state.polkadotConnected && chain.type === "substrate") {
       switchPolkadotChain(chain);
     }
-  }
+  };
 
   const queryChains = chains.filter((chain) => {
     if (state.ethereumConnected && chain.type === "evm") {
-      return chain.name.toLowerCase().startsWith(queryChain.toLowerCase())
-    }
-    
-    if (state.polkadotConnected && chain.type === "substrate") {
-      return chain.name.toLowerCase().startsWith(queryChain.toLowerCase())
+      return chain.name.toLowerCase().startsWith(queryChain.toLowerCase());
     }
 
-    if (!state.ethereumConnected && !state.polkadotConnected){
-      return chain.name.toLowerCase().startsWith(queryChain.toLowerCase())
+    if (state.polkadotConnected && chain.type === "substrate") {
+      return chain.name.toLowerCase().startsWith(queryChain.toLowerCase());
+    }
+
+    if (!state.ethereumConnected && !state.polkadotConnected) {
+      return chain.name.toLowerCase().startsWith(queryChain.toLowerCase());
     }
   });
 
   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
     router.push("/");
     event.preventDefault();
-    setUser({...user, chain: "", address: ""});
+    setUser({ ...user, chain: "", address: "" });
   };
 
   return (
-    <div id='switch_modal' onClick={handleClose} className="z-40 bg-[rgba(0,0,0,0.6)] w-[100vw] h-[100vh] flex fixed items-center justify-center inset-0">
+    <div
+      id="switch_modal"
+      onClick={handleClose}
+      className="z-40 bg-[rgba(0,0,0,0.6)] w-[100vw] h-[100vh] flex fixed items-center justify-center inset-0"
+    >
       <div className="z-50 md:w-[65vw] w-[95vw] h-[calc(80vh-16px)] bg-[#131313] flex flex-col items-center gap-[16px] rounded-[16px] border-2 border-[#303030] p-4">
         <div className="flex items-center justify-between w-full h-[calc(10vh-16px)] gap-2 p-2">
-          <div className="flex flex-row items-center justify-center w-full h-full p-2 bg-[#1a1a1a] rounded-[12px]">
+          <div className="flex flex-row items-center justify-center w-full h-full p-4 bg-[#1a1a1a] rounded-[12px]">
             <FiSearch className="text-[#9b9b9b] h-5 w-5" />
             <span className="h-2 w-2 mr-1 block shrink-0" />
-            <input 
+            <input
               type="search"
               className="bg-inherit w-full text-[#eaeaea] text-sm outline-none placeholder:text-[#5b5b5b] placeholder:text-sm"
               onChange={(e) => setQueryChain(e.target.value)}
-              placeholder="Search chains"/>
+              placeholder="Search chains"
+            />
           </div>
-          <div className="flex flex-row items-center justify-center p-2 bg-[#1b1b1b] rounded-[10px]" onClick={handleClick}>
-            <HeadlessSwitch checked={toggle} onChange={setToggle} className="relative inline-flex h-6 w-6 items-center rounded-full ">
+          <div
+            className="flex flex-row items-center justify-center p-2 bg-[#311c31] rounded-[10px]"
+            onClick={handleClick}
+          >
+            <HeadlessSwitch
+              checked={toggle}
+              onChange={setToggle}
+              className="relative inline-flex h-6 w-6 items-center rounded-full "
+            >
               <span className="sr-only text-white">Mode</span>
               <GiCycle
                 className={`${
-                  toggle ? 'rotate-0' : 'rotate-180' } inline-block h-10 w-10 transform text-[#656565] transition`} />
+                  toggle ? "rotate-0" : "rotate-180"
+                } inline-block h-10 w-10 transform text-[#ffffff] transition`}
+              />
             </HeadlessSwitch>
           </div>
           <div className="flex flex-row text-wrap text-center items-center space-x-[5px] bg-[#1a1a1a] p-2 rounded-[16px] w-full h-full cursor-pointer">
             <span className="h-2 w-2 mr-2 block shrink-0" />
-            {toggle 
-              ? <span className="text-[#9b9b9b] text-sm">Network Switcher</span> 
-              : <span className="text-[#9b9b9b] text-sm">Network Selector</span>}
+            {toggle ? (
+              <span className="text-[#9b9b9b] text-sm">Network Switcher</span>
+            ) : (
+              <span className="text-[#9b9b9b] text-sm">Network Selector</span>
+            )}
           </div>
           <button onClick={onClose}>
             <RxCross2 className="text-[#eaeaea] h-5 w-5 animate-pulse" />
           </button>
         </div>
         <div className="bg-black/30 w-full h-full p-2 flex flex-col gap-2 rounded-[12px] overflow-y-scroll">
-          {toggle 
-            ? <Switcher queryChains={queryChains} handleSwitch={handleSwitch} onClose={onClose} />
-            : <Selector queryChains={queryChains} onClose={onClose} />}
+          {toggle ? (
+            <Switcher
+              queryChains={queryChains}
+              handleSwitch={handleSwitch}
+              onClose={onClose}
+            />
+          ) : (
+            <Selector queryChains={queryChains} onClose={onClose} />
+          )}
         </div>
       </div>
     </div>
--- components/widgets/Selector.tsx
+++ components/widgets/Selector.tsx
@@ -1,43 +1,56 @@
 "use client";
-import { Chain } from '@/constants/config';
-import { useFaucetContext } from '@/context';
-import Image from 'next/image';
-import React, { Fragment } from 'react';
-import { RxCross2 } from 'react-icons/rx';
+import { Chain } from "@/constants/config";
+import { useFaucetContext } from "@/context";
+import Image from "next/image";
+import React, { Fragment } from "react";
+import { RxCross2 } from "react-icons/rx";
+import { IoMdCloseCircle } from "react-icons/io";
 
 interface SelectorProps {
   queryChains: Chain[];
   onClose: () => void;
-};
+}
 
-const Selector = ({ queryChains, onClose }: SelectorProps): React.JSX.Element => {
-  const {user, setUser, selectedChains, setSelectedChains} = useFaucetContext();
-  const westend_chains = queryChains.filter(chain => chain.chainType === "Westend & Parachain");
-  const rococo_chains = queryChains.filter(chain => chain.chainType === "Rococo & Parachain");
-  const paseo_chains = queryChains.filter(chain => chain.chainType === "Paseo & Parachain");
-  const solochains = queryChains.filter(chain => chain.chainType === "Solochain");
+const Selector = ({
+  queryChains,
+  onClose,
+}: SelectorProps): React.JSX.Element => {
+  const { user, setUser, selectedChains, setSelectedChains } =
+    useFaucetContext();
+  const westend_chains = queryChains.filter(
+    (chain) => chain.chainType === "Westend & Parachain"
+  );
+  const rococo_chains = queryChains.filter(
+    (chain) => chain.chainType === "Rococo & Parachain"
+  );
+  const paseo_chains = queryChains.filter(
+    (chain) => chain.chainType === "Paseo & Parachain"
+  );
+  const solochains = queryChains.filter(
+    (chain) => chain.chainType === "Solochain"
+  );
   console.log(selectedChains);
   const all_chains = [
     {
-      title: 'Westend & Parachain',
-      chains: westend_chains
+      title: "Westend & Parachain",
+      chains: westend_chains,
     },
     {
-      title: 'Rococo & Parachain',
-      chains: rococo_chains
+      title: "Rococo & Parachain",
+      chains: rococo_chains,
     },
     {
-      title: 'Paseo & Parachain',
-      chains: paseo_chains
+      title: "Paseo & Parachain",
+      chains: paseo_chains,
     },
     {
-      title: 'Solochains',
-      chains: solochains
-    }
+      title: "Solochains",
+      chains: solochains,
+    },
   ];
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {name, checked} = event.target;
+    const { name, checked } = event.target;
     if (checked) {
       setSelectedChains([...selectedChains, name]);
     } else {
@@ -47,7 +60,9 @@ const Selector = ({ queryChains, onClose }: SelectorProps): React.JSX.Element =>
 
   const handleClick = (chain: Chain) => {
     if (selectedChains.includes(chain.name)) {
-      setSelectedChains(selectedChains.filter((chain_name) => chain_name !== chain.name));
+      setSelectedChains(
+        selectedChains.filter((chain_name) => chain_name !== chain.name)
+      );
     } else {
       setSelectedChains([...selectedChains, chain.name]);
     }
@@ -58,44 +73,80 @@ const Selector = ({ queryChains, onClose }: SelectorProps): React.JSX.Element =>
     setUser({ ...user, chain: selectedChains.toLocaleString() });
     onClose();
   };
-  
+
   return (
-    <form id="chain-selector" className="flex flex-col items-center justify-center space-y-5 p-1" onSubmit={handleSubmit}>
+    <form
+      id="chain-selector"
+      className="flex flex-col items-center justify-center space-y-5 p-1"
+      onSubmit={handleSubmit}
+    >
       <div className="flex flex-col w-full space-y-1">
-      {all_chains.map(chain_group => (
-        <div key={chain_group.title} className="w-full p-1 space-y-1">
-          <div className="inline-block w-full">
-            <span className="text-[#9b9b9b] ml-2 p-2 text-sm">{chain_group.title}</span>
-            <div className="border-b-2 ml-2 border-[#303030]" />
-          </div>
-          <div className="grid text-sm gap-1 grid-cols-2 items-center font-bold">
-            {chain_group.chains.map(chain => (
-              <Fragment key={chain.name}>
-                {selectedChains.includes(chain.name) 
-                ?
-                  <div key={chain.url} className="text-[#9b9b9b] text-wrap text-center bg-[#131313] flex items-center justify-between ml-2 px-2 py-1 w-4/5 h-full rounded-md cursor-pointer">
-                    <div className="">{chain.name}</div>
-                    <span className="h-2 w-2 mr-2 flex shrink-0" />
-                    <RxCross2 className="h-4 w-4" onClick={() => handleClick(chain)} />
-                  </div>
-                :
-                  <div key={chain.url} className="text-[#dadada] text-wrap text-center bg-[#131313] flex items-center ml-2 px-2 py-1 h-full rounded-md cursor-pointer hover:bg-[#181818]" >
-                    <input type="checkbox" onClick={() => setUser({...user, chain: selectedChains.toLocaleString()})} value={chain.name} name={chain.name} onChange={handleChange} checked={selectedChains.includes(chain.name)} />
-                    <span className="h-1 w-1 mr-1 block shrink-0" />
-                    <div className="flex items-center justify-center">
-                      <Image src="/metamask.svg" alt={chain.name} width={20} height={20} className="h-6 w-6" />
-                      <span className="h-2 w-2 mr-2 flex shrink-0" />
-                      <span className="">{chain.name}</span>
+        {all_chains.map((chain_group) => (
+          <div key={chain_group.title} className="w-full p-1 space-y-1">
+            <div className="inline-block w-full">
+              <span className="text-[#9b9b9b] ml-2 p-2 text-sm">
+                {chain_group.title}
+              </span>
+              <div className="border-b-2 ml-2 border-[#303030]" />
+            </div>
+            <div className="grid text-sm gap-1 grid-cols-3 font-normal">
+              {chain_group.chains.map((chain) => (
+                <Fragment key={chain.name}>
+                  {selectedChains.includes(chain.name) ? (
+                    <div
+                      key={chain.url}
+                      className="text-[#fff] text-wrap text-center bg-[#c358c5] flex flex-auto items-center ml-2 px-2 py-1 h-full rounded-full cursor-pointer"
+                    >
+                      <IoMdCloseCircle
+                        className="h-5 w-5 mr-2 text-[#131313]"
+                        onClick={() => handleClick(chain)}
+                      />
+                      <div className="">{chain.name}</div>
                     </div>
-                  </div>
-                }                
-              </Fragment>
-            ))}
-          </div> 
-        </div>
-      ))}
+                  ) : (
+                    <div
+                      key={chain.url}
+                      className="text-[#dadada] text-wrap text-center bg-[#131313] flex items-center ml-2 px-2 py-1 h-full rounded-md cursor-pointer hover:bg-[#181818]"
+                    >
+                      <input
+                        type="checkbox"
+                        onClick={() =>
+                          setUser({
+                            ...user,
+                            chain: selectedChains.toLocaleString(),
+                          })
+                        }
+                        value={chain.name}
+                        name={chain.name}
+                        onChange={handleChange}
+                        checked={selectedChains.includes(chain.name)}
+                      />
+                      <span className="h-1 w-1 mr-1 block shrink-0" />
+                      <div className="flex items-center justify-center">
+                        <Image
+                          src="/metamask.svg"
+                          alt={chain.name}
+                          width={20}
+                          height={20}
+                          className="h-6 w-6"
+                        />
+                        <span className="h-2 w-2 mr-2 flex shrink-0" />
+                        <span className="">{chain.name}</span>
+                      </div>
+                    </div>
+                  )}
+                </Fragment>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
-      <button className="w-1/4 text-[#0066ff] bg-[rgba(0,102,255,0.1)] active:scale-95 p-2 rounded-[12px]" type='submit'>Submit</button>
+      <button
+        className="w-1/4 text-[#fff] bg-[rgba(0,102,255,0.8)] active:scale-95 p-2 rounded-[12px]"
+        type="submit"
+      >
+        Submit
+      </button>
     </form>
   );
 };
