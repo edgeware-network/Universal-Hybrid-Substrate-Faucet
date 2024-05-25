a034c9697ea281892c5a26e97c6b64a415f9d4a5
pranavcpawar <pranavcpawar@outlook.com>
2024-05-25 18:42:13 +0530

    modify toast component

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -11,11 +11,11 @@ const Toast = ({ t, Icon, className, message }: ToastProps): React.JSX.Element =
     <div
     className={`${
       t.visible ? "animate-enter" : "animate-leave"
-    } max-w-[540px] w-full bg-[#202020] shadow-lg rounded-lg items-center border-[#404040] justify-center gap-[10px] pointer-events-auto flex ring-1 p-4 ring-black ring-opacity-5`}
+    } max-w-[540px] w-full bg-[#050505] shadow-lg text-[#ffffff] rounded-lg items-center border border-[#303030] justify-start pointer-events-auto flex ring-1 p-4 ring-black ring-opacity-5`}
   >
     <Icon className={className} />
     <span className="h-2 w-2 mr-2 block shrink-0" />
-      <p className="text-sm">{message}</p>
+      <p className="text-sm text-[#9b9b9b]">{message}</p>
   </div>
   );
 };
