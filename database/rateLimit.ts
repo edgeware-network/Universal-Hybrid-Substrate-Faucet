ea38b20525a6e9d2eff7e25a54adc4f4e89475f7
pranavcpawar <pranavcpawar@outlook.com>
2024-05-25 18:42:55 +0530

    remove old ratelimit logic

--- database/rateLimit.ts
+++
@@ -1,9 +0,0 @@
-import { Ratelimit } from "@upstash/ratelimit";
-import redis from "./redisDB";
-
-const ratelimit = new Ratelimit({
-  redis: redis,
-  limiter: Ratelimit.slidingWindow(1, "86400 s"),
-});
-
-export default ratelimit;
