7a6d9f39c50f00ebc431ed3766fed90d89dfdd33
Rama Vats <guy55222@gmail.com>
2024-06-01 09:18:45 +0530

    fix gray bg while connecting wallet.

--- components/widgets/ConnectingPopup.tsx
+++ components/widgets/ConnectingPopup.tsx
@@ -1,28 +1,31 @@
 import React from "react";
 import ClipLoader from "react-spinners/ClipLoader";
+import ParticlesComponent from "../ParticlesComponent";
 
 export default function ConnectingPopup() {
   return (
-    <div className="connect-modal bg-transparent w-[432px] h-[calc(100vh-16px)] fixed left-auto right-0 mt-1 mr-2 inset-0 flex overflow-hidden rounded-[16px]">
-      <div className="text-white w-[80px] hover:bg-[#4040402a] p-2"></div>
-      <div className="bg-[#131313] w-full p-4 h-full flex flex-col gap-[20px] items-center border-2 border-[#252525] rounded-[12px]">
-        <div className="flex items-center justify-between w-full">
-          <h3 className="text-[#eaeaea] text-base">
-            Please confirm the connection...
-          </h3>
-        </div>
-        <div className="w-full flex flex-col gap-[10px] items-center">
-          <ClipLoader
-            color="#FC72FF"
-            loading={true}
-            size={20}
-            speedMultiplier={1}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />{" "}
-          <span>Connecting... </span>
+    <>
+      <ParticlesComponent />
+      <div className="connect-modal w-full h-full fixed inset-0 flex items-center justify-center">
+        <div className="bg-[#131313] w-[432px] p-4 h-auto flex flex-col gap-[20px] items-center border-2 border-[#252525] rounded-[12px]">
+          <div className="flex items-center justify-between w-full">
+            <h3 className="text-[#eaeaea] text-base">
+              Please confirm the connection...
+            </h3>
+          </div>
+          <div className="w-full flex flex-col gap-[10px] items-center">
+            <ClipLoader
+              color="#FC72FF"
+              loading={true}
+              size={20}
+              speedMultiplier={1}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />{" "}
+            <span className="text-white">Connecting... </span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
