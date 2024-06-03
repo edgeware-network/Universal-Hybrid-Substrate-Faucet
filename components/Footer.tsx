7b78b7acd6143688a35a3be3effbc95e187b9a08
Rama Vats <guy55222@gmail.com>
2024-06-03 15:19:00 +0530

    fixed favicon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Footer = (): React.JSX.Element => {
   return (
-    <div className='max-w-[1204px] w-[90vw] fixed bottom-5 h-14 flex flex-col items-center text-wrap justify-center bg-[#1b1b1b] font-medium rounded-[10px]'>
-      <p className='text-xs font-medium text-[#656565] flex items-center justify-center'>Made by Edgeware contributors</p>
-      <p className='text-xs font-medium text-[#494949]'>Edgeware © 2024 - {new Date().getFullYear()}</p>
+    <div className="max-w-[1204px] w-[90vw] fixed bottom-5 h-14 flex flex-col items-center text-wrap justify-center bg-[#1b1b1b] font-medium rounded-[10px]">
+      <p className="text-xs font-medium text-[#656565] flex items-center justify-center">
+        Made with&nbsp;
+        <span className="text-pink-600 animate-pulse text-lg">💓</span>&nbsp;by
+        Edgeware contributors
+      </p>
+
+      <p className="text-xs font-medium text-[#494949]">
+        Edgeware © 2024 - {new Date().getFullYear()}
+      </p>
     </div>
   );
 };
